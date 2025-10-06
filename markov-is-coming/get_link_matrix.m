b3e9b5e95f1a9fd3ba46322f5ce3f65b1516890c
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:02:25 +0300

    Update get_link_matrix.m

--- markov-is-coming/get_link_matrix.m
+++ markov-is-coming/get_link_matrix.m
@@ -1,9 +1,5 @@
 function [Link] = get_link_matrix (Labyrinth)
-	% Labyrinth -> the matrix of all encodings
-  
-  % Link -> the link matrix associated to the given labyrinth
-  
-  % TODO: get_link_matrix implementation
+	
   Adj = get_adjacency_matrix(Labyrinth);
 
     [rows, cols] = size(Adj);
