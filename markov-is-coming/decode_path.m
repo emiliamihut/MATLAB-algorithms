f30768f0c21aed39761e89f228d2257a75fa4c97
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:01:19 +0300

    Update decode_path.m

--- markov-is-coming/decode_path.m
+++ markov-is-coming/decode_path.m
@@ -1,14 +1,4 @@
 function [decoded_path] = decode_path (path, lines, cols)
-	% path -> vector containing the order of the states that arrived
-	% 		 to a winning position
-	% lines -> numeber of lines
-	% cols -> number of columns
-
-	% decoded_path -> vector of pairs (line_index, column_index)
-  %                 corresponding to path states
-  % hint: decoded_path does not contain indices for the WIN state
-
-  % TODO: decode_path implementation
 
   WIN_idx = lines * cols + 1;
 
