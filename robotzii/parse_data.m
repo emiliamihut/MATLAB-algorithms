4343480c6359c20ecd367565ef4c362bba9c793b
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:04:55 +0300

    Update parse_data.m

--- robotzii/parse_data.m
+++ robotzii/parse_data.m
@@ -1,10 +1,5 @@
 function [x, y] = parse_data(filename)
-    % file input format:
-    % n
-    % x0 x1 ... xn
-    % y0 y1 ... yn
-
-    % TODO 1: Open the file for reading
+   
     fid = fopen(filename, 'r');
     if fid == -1
         error('error opening input file');
