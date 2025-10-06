98f14d9caf928b7f0470ab51c53bfd1ae22a424b
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:04:06 +0300

    Update P_spline.m

--- robotzii/P_spline.m
+++ robotzii/P_spline.m
@@ -1,11 +1,5 @@
 function y_interp = P_spline(coef, x, x_interp)
-    % si(x)   = ai + bi(x - xi) + ci(x - xi)^2 + di(x - xi)^3, i = 0 : n - 1
-    % coef = [a0, b0, c0, d0, a1, b1, c1, d1, ..., an-1, bn-1, cn-1, dn-1]
-    % x = [x0, x1, ..., xn]
-    % y_interp(i) = P_spline(x_interp(i)), i = 0 : length(x_interp) - 1
-    % Be careful! Indexes in Matlab start from 1, not 0
-
-    % TODO: Calculate y_interp using the Spline coefficients
+   
 
     x = x(:);          % make sure x is a column vector
     xi = x_interp(:);  % make sure x_interp is a column vector
