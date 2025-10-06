e73ebeb24f3896d8dd98d71a616c4380f90facd0
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:01:38 +0300

    Update get_Jacobi_parameters.m

--- markov-is-coming/get_Jacobi_parameters.m
+++ markov-is-coming/get_Jacobi_parameters.m
@@ -1,11 +1,5 @@
 function [G, c] = get_Jacobi_parameters (Link)
-	% Link -> the link matrix (edge with probabilities matrix)
-
-	% G -> iteration matrix
-	% c -> iteration vector
-
-	% TODO: get_Jacobi_parameters
-
+	
 	N = size(Link, 1) - 2;  % just transitory states
 	WIN = N + 1;
 	LOSE = N + 2;
