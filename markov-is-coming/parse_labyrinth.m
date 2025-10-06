e0c2c12810ec47fe85b316c0d81992d9f03010a5
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:02:57 +0300

    Update parse_labyrinth.m

--- markov-is-coming/parse_labyrinth.m
+++ markov-is-coming/parse_labyrinth.m
@@ -1,10 +1,5 @@
 function [Labyrinth] = parse_labyrinth(file_path)
-	% file_path -> the relative path to a file that needs to
-  %              be loaded_graphics_toolkits
-
-  % Labyrinth -> the matrix of all encodings for the labyrinth's walls
-
-  % TODO: parse_labyrinth implementation
+	
   fid = fopen(file_path, 'r');
 
   dims = fscanf(fid, '%d', 2);
