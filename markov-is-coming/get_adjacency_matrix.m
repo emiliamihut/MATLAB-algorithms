e54ad5810e3dbb56879de5205e861b8e56c37c9a
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:02:07 +0300

    Update get_adjacency_matrix.m

--- markov-is-coming/get_adjacency_matrix.m
+++ markov-is-coming/get_adjacency_matrix.m
@@ -1,10 +1,4 @@
 function [Adj] = get_adjacency_matrix(Labyrinth)
-
-  % Labyrinth -> the matrix of all encodings
-  
-  % Adj -> the adjacency matrix associated to the given labyrinth
-  
-  % TODO: get_adjacency_matrix implementation
   
   [m, n] = size(Labyrinth);
   N = m * n;
