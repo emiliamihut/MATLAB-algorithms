8069c823969e39ead9987f8abe22eb0e93da3760
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:00:05 +0300

    Update parse_data_set_file.m

--- linear-regression/parse_data_set_file.m
+++ linear-regression/parse_data_set_file.m
@@ -1,10 +1,4 @@
 function [Y, InitialMatrix] = parse_data_set_file(file_path)
-    % path -> a relative path to the .txt file
-
-    % Y -> the vector with all actual values
-    % InitialMatrix -> the matrix that must be transformed
-    
-    % TODO: parse_data_set_file implementation
 
     fid = fopen(file_path, 'r');
 
