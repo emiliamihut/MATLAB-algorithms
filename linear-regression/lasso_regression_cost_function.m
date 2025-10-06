277d79f94d25810e74aa1f73433418583ce8e23d
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 20:58:32 +0300

    Update lasso_regression_cost_function.m

--- linear-regression/lasso_regression_cost_function.m
+++ linear-regression/lasso_regression_cost_function.m
@@ -1,13 +1,5 @@
 function [Error] = lasso_regression_cost_function(Theta, Y, FeatureMatrix, lambda)
-  % Theta -> the vector of weights
-  % Y -> the vector with all actual values
-  % FeatureMatrix -> the matrix with all training examples
-  % lambda -> regularization parameter that controls the amount of
-  %           shrinkage applied to the regression coefficients
-
-  % Error -> the error of the regularized cost function
-
-  % TODO: lasso_regression_cost_function implementation
+  
   m = size(Y, 1);
   X = [ones(m, 1), FeatureMatrix]; % add a column of ones to X
   htheta = X * Theta;
