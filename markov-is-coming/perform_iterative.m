e9217ca8afcd93e4f09498aacd83dbbef63654f6
Maria-Emilia Mihut <65251016+emiliamihut@users.noreply.github.com>
2025-10-06 21:03:16 +0300

    Update perform_iterative.m

--- markov-is-coming/perform_iterative.m
+++ markov-is-coming/perform_iterative.m
@@ -1,15 +1,5 @@
 function [x, err, steps] = perform_iterative (G, c, x0, tol, max_steps)
-	% G -> iteration matrix
-	% c -> iteration vector
-	% x0 -> initial solution
-	% tol -> accepted tolerance (norm of the difference between two solutions)
-	% max_steps -> the maximum number of iterations
-
-	% x -> final solution
-	% err -> last calculated difference (as an euclidean norm)
-	% steps -> actual number of steps performed
-
-   % TODO: perform_iterative implementation
+	
 	prev = x0;
 	for steps = 1:max_steps
 
